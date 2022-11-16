c0ca7bb82fb19953806d2e766f7a82c146ca06d8
Meraj Rizwan <meraj.rizwan9832@gmail.com>
2022-11-16 08:04:53 +0530

    productScreen cartScreen

--- frontend/src/Screens/CartScreen.jsx
+++ frontend/src/Screens/CartScreen.jsx
@@ -1,8 +1,27 @@
-import './CartScreen.css'
+import './CartScreen.css';
+import CartItem from '../components/CartItem';
 const CartScreen = () => {
     return (
-        <div className='cartScreen'>
-            CartScreen
+        <div className='cartscreen'>
+            <div className="cartscreen_left">
+                <h2>Shopping Cart</h2>
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+
+            </div>
+            <div className="cartscreen_right">
+                <div className="cartscreen_info">
+                    <p>Subtotal (0) items</p>
+                    <p>RS 499.99</p>
+                </div>
+                <div>
+                    <button>Proceed To Checkout</button>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ frontend/src/components/CartItem.jsx
@@ -0,0 +1,30 @@
+import './CartItem.css';
+import {Link} from 'react-router-dom';
+
+const CartItem = () => {
+  return (
+    <div className='cartitem'>
+        <div className="cartitem_image">
+        <img src="https://imgnew.outlookindia.com/uploadimage/library/16_9/16_9_5/IMAGE_1660025387.webp" alt='product img'/>
+        </div>
+        <Link to={`/product/${1111}`} className='cartitem_name'>
+            <p>Product 1</p>
+        </Link>
+        <p className="cartitem_price">Rs 499.99</p>
+        <select className='cartitem_select'>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+
+        </select>
+        <button className="cartitem_deleteBtn">
+            <i className="fas fa-trash"></i>
+        </button>
+
+
+    </div>
+  )
+}
+
+export default CartItem
